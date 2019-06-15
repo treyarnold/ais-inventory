90d9d2b97a6909843f655c791aee05eac432227c
Ibrahim Iqbal <iaiqbal@uh.edu>
2019-06-15 12:43:50 -0500

    modifiying the POS system page

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,1 +1,8 @@
 const queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(data) {
+    console.log("this is the response: " + data.strDrink);
+})
