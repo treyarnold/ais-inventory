eb2ea2203948be8162607a67d253ebca1d724e4a
Chris Stroupe <cstroupe79@gmail.com>
2019-06-16 11:26:42 -0500

    changed tables to all work with routes

---
+++ seeders/inventory.sql
@@ -0,0 +1,24 @@
+use ais_db;
+INSERT INTO inventories (name, type, quantity_bottles, price_bottle, inventory_value) VALUES
+('Smirnoff', 'Vodka', 20, 25, 500),
+('Absolut', 'Vodka', 15, 35, 525),
+('Skyy', 'Vodka', 10, 27, 270),
+('Grey Goose', 'Vodka', 5, 50, 250),
+('Jack Daniels', 'Whiskey', 15, 27, 405),
+('Makers Mark', 'Whiskey', 10, 30, 300),
+('Jameson', 'Whiskey', 5, 30, 150),
+('Jim Beam', 'Whiskey', 10, 15, 150),
+('Johnnie Walker Black', 'Scotch', 5, 60, 300),
+('Johnnie Walker Blue', 'Scotch', 2, 165, 330),
+('Glenfiddich', 'Scotch', 5, 25, 125),
+('Glenlivet', 'Scotch', 5, 30, 150),
+('Singleton', 'Scotch', 5, 30, 150),
+('Tanqueray', 'Gin', 5, 35, 175),
+('Beefeater', 'Gin', 10, 25, 250),
+('Gordons', 'Gin', 10, 15, 150),
+('Hendricks', 'Gin', 2, 65, 130),
+('Don Julio', 'Tequila', 5, 55, 275),
+('Patron', 'Tequila', 10, 40, 400),
+('Jose Cuervo', 'Tequila', 10, 20, 200),
+('Avion', 'Tequila', 5, 35, 175);
+
