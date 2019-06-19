1d8ef24a275ee463074492850c1bd53e735cc65f
treyarnold <arnold.trey80@gmail.com>
2019-06-18 20:18:49 -0500

    scanner working with logging out the upc on new read

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,30 +10,9 @@ module.exports = function(app) {
   })
 
   app.get("/", function(req, res) {
-    console.log(req);
-    // db.Example.findAll({}).then(function(dbExamples) {
-    //   res.render("index", {
-    //     msg: "Welcome!",
-    //     examples: dbExamples
-    //   });
-    // });
-    res.render("login");
+    res.render("login", {user: req.user});
   });
-
-  // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   // db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  //   //   res.render("example", {
-  //   //     example: dbExample
-  //   //   });
-  //   // });
-  // });
-
-  // Render 404 page for any unmatched routes
-  // app.get("*", function(req, res) {
-  //   // res.render("404");
-  // });
-
+  
   app.get("/scanner", isAuthenticated, function(req, res) {
     res.render("scanner", {user: req.user});
   });
@@ -49,4 +28,9 @@ module.exports = function(app) {
   app.get("/login", function(req, res) {
     res.render("login", {user: req.user});
   });
+  
+  // Render 404 page for any unmatched routes
+  // app.get("*", function(req, res) {
+  //   // res.render("404");
+  // });
 };
