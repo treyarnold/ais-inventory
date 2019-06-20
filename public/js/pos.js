ec13f54287c598778cd46cc067edb77c5e236e1e
treyarnold <arnold.trey80@gmail.com>
2019-06-20 16:38:53 -0500

    clicked orders now added to order sheet

--- public/js/pos.js
+++ public/js/pos.js
@@ -3,9 +3,20 @@ $(document).ready(() => {
   $.each(options, (idx, element) => {
     $.get(`/api/drinks/${element.id}`, (dbdrink) => {
       dbdrink.forEach(ingredient => {
-        const button = `<a class="dropdown-item liquorChoice" href="#">${ingredient.name}</a>`;
+        const button = `<a class="dropdown-item liquorChoice" drink=${element.id}>${ingredient.name}</a>`;
         $(`#${element.id}`).append(button);
       });
     });
   });
+});
+
+$(document).on('click', '.liquorChoice', function (event) {
+  // console.log($(event.target).parent().parent().attr("id"));
+  // console.log(event.target.attributes.drink.value);
+  // console.log(event.target.text);
+  // console.log(dbdrink);
+  const order = `<p>${$(event.target).parent().parent().attr("id")} - ${event.target.text}</p>`;
+  console.log(order);
+  console.log($("#currentOrder"));
+  $("#currentOrder").append(order);
 });
